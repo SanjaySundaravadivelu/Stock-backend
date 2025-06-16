c2fa4d69cb73fbc869328621ac7b14a9c3e22860
SanjaySundaravadivelu <157513859+SanjaySundaravadivelu@users.noreply.github.com>
2025-06-16 17:20:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(mongourl);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1); // process code 1 code means exit with failure, 0 means success
